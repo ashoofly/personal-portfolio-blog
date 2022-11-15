052456d96cdccbf6a8c2ac72ef88f6f375ed9225
Angela Hsu <ayh123@gmail.com>
2022-11-14 20:41:10 -0700

    sharing meta tags

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -24,13 +24,11 @@ export default function Layout(props: LayoutProps) {
         <link rel="manifest" href="/site.webmanifest" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="that Software Engineer life"
         />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content={`https://personal-portfolio-blog-ashoofly.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fartist.4c52869d.png&w=384&q=75`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
